35fe4e558afb9f93c3732585287083920d8a759b
Eloisa Potrich <30781570+EloPo@users.noreply.github.com>
2026-01-13 23:27:48 -0300

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,12 +1,5 @@
-import os
-from telegram import Update
-from bot_telegram_ri import criar_app
-
-TOKEN = os.environ["BOT_TOKEN"]
-app = criar_app(TOKEN)
-
-async def handler(request):
-    data = await request.json()
-    update = Update.de_json(data, app.bot)
-    await app.process_update(update)
-    return {"status": "ok"}
+def handler(request):
+    return {
+        "status": "ok",
+        "message": "Webhook ativo"
+    }
