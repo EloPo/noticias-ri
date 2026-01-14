6ed193041a0545b90b4bf3ba06abfab3855ddf54
Eloisa Potrich <30781570+EloPo@users.noreply.github.com>
2026-01-13 23:47:13 -0300

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,5 +1,15 @@
-def handler(request):
-    return {
-        "status": "ok",
-        "message": "Webhook ativo"
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        response = {
+            "status": "ok",
+            "message": "Webhook ativo"
+        }
+
+        self.wfile.write(json.dumps(response).encode())
